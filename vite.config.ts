dc2c6ab2060fc1a2ed043e70c7f589e3fb589475
atasker <alex.tasker727@gmail.com>
2025-08-27 19:09:35 +0100

    argh

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import sass from 'sass'
 
+console.log(process.env.VITE_HOST_URL)
+
 export default {
     plugins: [react()],
     resolve: {
@@ -19,6 +21,7 @@ export default {
     },
     build: {
       allowedHosts: [process.env.VITE_HOST_URL],
+      allowedHost: [process.env.VITE_HOST_URL],
       proxy: {
         [process.env.VITE_API_PREFIX ?? '/api']: new URL(process.env.VITE_BACKEND_URL ?? 'http://localhost:3000').origin,
       },
@@ -26,9 +29,11 @@ export default {
     },
     preview: {
       allowedHosts: [process.env.VITE_HOST_URL],
+      allowedHost: [process.env.VITE_HOST_URL],
       proxy: {
         [process.env.VITE_API_PREFIX ?? '/api']: new URL(process.env.VITE_BACKEND_URL ?? 'http://localhost:3000').origin,
       },
+      changeOrigin: true
     },
     server: {
       port: process.env.VITE_APP_PORT,
