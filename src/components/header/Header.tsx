9741d18a7e425cecf98846d1f25198299e9b9e8f
atasker <alex.tasker727@gmail.com>
2025-08-27 16:39:33 +0100

    header & layout tweaks

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,7 +3,7 @@ import type { JSX } from 'react';
 export default function Header(): JSX.Element {
   return (
     <div className="application-header m-0">
-      <h1 className="application-header-title text-grey h-100">Space Photo Viewer (name WIP)</h1>
+      <h1 className="application-header-title text-grey h-100">Space Photo Viewer</h1>
     </div>
   );
 }
